63f2983539aacf4d249aeb758b594e6e138fd005
Rodolfomno <Rodolfo_mno@hotmail.com>
2022-04-24 13:08:17 -0300

    feat: adiciona resolucao requisito 2

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,3 +1,9 @@
 import Race from './Race';
+import Elf from './Elf';
+import Halfling from './Halfling';
+import Dwarf from './Dwarf';
+import Orc from './Orc';
 
 export default Race;
+
+export { Elf, Halfling, Dwarf, Orc };
